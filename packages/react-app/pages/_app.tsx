6e958268f7ec48667883276ae226148f9e458fe6
yionidas <yassine.nc23@gmail.com>
2024-03-16 00:09:48 +0100

    Basic Setup

--- packages/react-app/pages/_app.tsx
+++ packages/react-app/pages/_app.tsx
@@ -1,11 +1,16 @@
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+import PrimaryButton from "../components/Button";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <PrimaryButton
+        title="Go to Home Page"
+        onClick={() => console.log("Go to Home Page")}
+      />
     </Layout>
   );
 }
